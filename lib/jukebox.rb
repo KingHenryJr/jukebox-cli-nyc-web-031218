0cca7ac864381af5b7b36090bd2e1d959a5be651
Henry Zhang <henryyihengzhang@gmail.com>
2018-03-02 17:47:44 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,7 +10,7 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def help(songs) 
+def help
  help = "I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
